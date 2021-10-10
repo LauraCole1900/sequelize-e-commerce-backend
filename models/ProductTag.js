a4ec6006278b1810a284d29a87eb41575db54bde
Laura Cole <lauracole1900@comcast.net>
2021-10-10 14:27:06 -0600

    defined most of the ProductTag model

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,6 +7,20 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      // References the `Product` model's `id`
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      // References the `Tag` model's `id`
+    }
   },
   {
     sequelize,
