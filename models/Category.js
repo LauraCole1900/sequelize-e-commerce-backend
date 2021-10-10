c2e5458f3514ad523a15df3926f64910f75fb805
Laura Cole <lauracole1900@comcast.net>
2021-10-10 14:02:15 -0600

    transferred category info from schema to model

--- models/Category.js
+++ models/Category.js
@@ -6,7 +6,16 @@ class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   },
   {
     sequelize,
